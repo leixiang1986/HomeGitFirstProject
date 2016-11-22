2b752e442e066c24e8b4a930fdd85e3efbdc09b6
leixiang <541150547@qq.com>
2016-11-22 23:44:39 +0800

    Initial Commit

---
+++ HomeGitProjectFirst/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HomeGitProjectFirst
+//
+//  Created by mac on 16/11/22.
+//  Copyright © 2016年 leixiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HomeGitProjectFirst/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HomeGitProjectFirst
+//
+//  Created by mac on 16/11/22.
+//  Copyright © 2016年 leixiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
